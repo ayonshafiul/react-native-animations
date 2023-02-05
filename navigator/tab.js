a5f8d2315f862af4f01ec4d5fe1b73a2b6246166
shaheen2013 <coredeveloper.2013@gmail.com>
2023-02-05 17:53:55 +0600

    Basics of reanimated

--- navigator/tab.js
+++ navigator/tab.js
@@ -1,43 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-
 import { View, Text, StyleSheet, ScrollView } from "react-native";
-import Animated, {
-  withSpring,
-  withTiming,
-  useSharedValue,
-  useAnimatedStyle,
-  withRepeat,
-} from "react-native-reanimated";
-import { useEffect } from "react";
-
-function Home() {
-  let x = useSharedValue(0);
-
-  const reanimatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ scale: x.value }],
-    };
-  });
-  useEffect(() => {
-    x.value = withRepeat(withSpring(1.2), -1, true);
-  }, []);
-  return (
-    <ScrollView
-      style={[s.container]}
-      contentContainerStyle={{
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Animated.View style={[s.box, reanimatedStyle]}></Animated.View>
-      <Animated.View style={[s.box, reanimatedStyle]}></Animated.View>
-      <Animated.View style={[s.box, reanimatedStyle]}></Animated.View>
-      <Animated.View style={[s.box, reanimatedStyle]}></Animated.View>
-      <Animated.View style={[s.box, reanimatedStyle]}></Animated.View>
-    </ScrollView>
-  );
-}
+import Home from "../screens/Home";
+import About from "../screens/About";
 
 export default function Tab() {
   const Tab = createBottomTabNavigator();
@@ -47,21 +12,10 @@ export default function Tab() {
         showLabel: false,
       }}
     >
-      <Tab.Screen name="Hello" component={Home} />
-      <Tab.Screen name="About" component={Home} />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="About" component={About} />
     </Tab.Navigator>
   );
 }
 
-const s = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: "red",
-    borderRadius: 20,
-    margin: 20,
-  },
-});
+const s = StyleSheet.create({});
---
+++ screens/Home.js
@@ -0,0 +1,42 @@
+import { StyleSheet, View } from 'react-native'
+import React, { useEffect } from 'react'
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated'
+
+export default function Home() {
+    const scale = useSharedValue(0)
+
+    const calculateRotation = (value) => {
+        'worklet';
+        return `${value * 2 * Math.PI}rad`
+    }
+    const scalingView = useAnimatedStyle(() => {
+        return {
+            transform: [{ scale: scale.value }, { rotate: calculateRotation(scale.value) }]
+        }
+    }, [])
+
+    useEffect(() => {
+        scale.value = withRepeat(withTiming(1), -1, true);
+    }, [])
+    return (
+        <View style={s.container}>
+            <Animated.View style={[s.box, scalingView]}>
+
+            </Animated.View>
+        </View>
+    )
+}
+
+const s = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    box: {
+        width: 200,
+        height: 200,
+        backgroundColor: 'purple',
+        borderRadius: 8
+    }
+})

df5e0344e99463f2f43bb76353be2b3258da419e
ayonshafiul <ayonshafiul@gmail.com>
2023-02-05 19:40:11 +0600

    Add interpolate screen

--- navigator/tab.js
+++ navigator/tab.js
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 import Home from "../screens/Home";
+import Interpolate from "../screens/Interpolate";
 import PanGesture from "../screens/PanGesture";
 
 export default function Tab() {
@@ -14,6 +15,7 @@ export default function Tab() {
     >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Pan Gesture" component={PanGesture} />
+      <Tab.Screen name="Interpolate" component={Interpolate} />
     </Tab.Navigator>
   );
 }

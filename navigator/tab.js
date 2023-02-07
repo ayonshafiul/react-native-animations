2fedfb042c497723fe87264d805ab76a91869ad9
ayonshafiul <ayonshafiul@gmail.com>
2023-02-07 09:11:07 +0600

    Add drawer navigator

--- navigator/tab.js
+++ navigator/tab.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import React from "react";
 import DoubleTap from "../screens/DoubleTap";
 import Home from "../screens/Home";
@@ -8,18 +9,19 @@ import PanGesture from "../screens/PanGesture";
 
 export default function Tab() {
   const Tab = createBottomTabNavigator();
+  const Drawer = createDrawerNavigator();
   return (
-    <Tab.Navigator
+    <Drawer.Navigator
       screenOptions={{
         showLabel: false,
       }}
     >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Pan Gesture" component={PanGesture} />
-      <Tab.Screen name="Interpolate" component={Interpolate} />
-      <Tab.Screen name="Interpolate Color" component={InterpolateColor} />
-      <Tab.Screen name="Double Tap" component={DoubleTap} />
-    </Tab.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Pan Gesture" component={PanGesture} />
+      <Drawer.Screen name="Interpolate" component={Interpolate} />
+      <Drawer.Screen name="Interpolate Color" component={InterpolateColor} />
+      <Drawer.Screen name="Double Tap" component={DoubleTap} />
+    </Drawer.Navigator>
   );
 }
 
